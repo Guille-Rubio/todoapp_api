87bc5a08dcbb76e7dd40649efac2de2fd40584b0
Guille-Rubio <guillermorubiog@gmail.com>
2023-01-25 23:06:08 +0100

    FIX hardcoded url in google passport

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -1,24 +1,25 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
+const baseUrl = require('../utils/environment');
 
 //Establecemos la estrategia de Google con los credenciales de nuestro proyecto
 passport.use(new GoogleStrategy({
-    clientID: `${process.env.GOOGLE_OAUTH_IDCLIENT}`,
-    clientSecret: `${process.env.GOOGLE_OAUTH_SECRET}`,
-    callbackURL: `http://localhost:5000/google/callback`,
-    proxy: true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
+  clientID: `${process.env.GOOGLE_OAUTH_IDCLIENT}`,
+  clientSecret: `${process.env.GOOGLE_OAUTH_SECRET}`,
+  callbackURL: `${baseUrl}/google/callback`,
+  proxy: true
+},
+  function (request, accessToken, refreshToken, profile, done) {
     return done(null, profile);
   }
 ));
 
 //Esta función determina los datos que se van a guardar en la sesión de google: user
 passport.serializeUser(function (user, done) {
-    done(null,user)
+  done(null, user)
 });
 //Determina que objeto borrar de la sesión: user
 passport.deserializeUser(function (user, done) {
-    done(null,user)
+  done(null, user)
 });
