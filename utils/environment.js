3f9fab3ffaf3e2655fe74ce58fcf734f273627f5
Guille-Rubio <guillermorubiog@gmail.com>
2023-01-25 23:00:59 +0100

    UPDATED removed / from baseurl

--- utils/environment.js
+++ utils/environment.js
@@ -3,7 +3,7 @@ let baseUrl
 if (process.env.NODE_ENV === 'production') {
     baseUrl = "to-do-app-gray-two.vercel.app"
 } else {
-    baseUrl = "http://localhost:3000/"
+    baseUrl = "http://localhost:3000"
 }
 
 module.exports = baseUrl;
