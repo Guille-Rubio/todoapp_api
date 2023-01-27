3422371e4bff4938b04c531bd7d391bc6713d045
Guille-Rubio <guillermorubiog@gmail.com>
2023-01-27 13:41:36 +0100

    removed mongoose connection error handling

--- config/mongoDBconnection.js
+++ config/mongoDBconnection.js
@@ -8,22 +8,21 @@ const configParams = {
     sslValidate: false,
 };
 
-(() => {
-    try {
-        mongoose.connect(process.env.MONGO_DB_CONNECTION, configParams);
 
-        const db = mongoose.connection;
 
-        db.on("error", console.error.bind(console, "connection error: "));
-        db.once("open", function () {
-            console.log("Mongo DB Connected successfully");
-        });
-    } catch (error) {
-        console.log(error.message);
-        throw error;
-    }
+mongoose.connect(process.env.MONGO_DB_CONNECTION, configParams);
 
-})();
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Mongo DB Connected successfully");
+});
+
+
+
+
+
 
 
 module.exports = mongoose
